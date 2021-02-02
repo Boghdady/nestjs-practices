944b5813969db6dc9bd669d8a60b0af55caa701b
boghdady <boghdady107@gmail.com>
2021-02-02 23:30:48 +0200

    Class providers

--- src/coffee/coffee.module.ts
+++ src/coffee/coffee.module.ts
@@ -1,16 +1,27 @@
 import { Module } from '@nestjs/common';
 
-import { COFFEE_BRAND } from '../common/constants';
 import { CoffeeController } from './coffee.controller';
 import { CoffeeService } from './coffee.service';
 
 class MockCoffeeService {}
 
+// useClass provider
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
+
 @Module({
   // providers: [{ provide: CoffeeService, useValue: new MockCoffeeService() }],
   providers: [
     CoffeeService,
-    { provide: COFFEE_BRAND, useValue: ['Turky', 'French'] },
+    // { provide: COFFEE_BRAND, useValue: ['Turky', 'French'] },
+    {
+      provide: CoffeeService,
+      useClass:
+        process.env.NODE_ENV === 'developement'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
+    },
   ],
   controllers: [CoffeeController],
   // exports: [CoffeeService],
