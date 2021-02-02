eec2d35d7e35249ed5e706923eed1caf99ea9332
boghdady <boghdady107@gmail.com>
2021-02-02 23:39:48 +0200

    useFactory provider

--- src/coffee/coffee.module.ts
+++ src/coffee/coffee.module.ts
@@ -1,26 +1,45 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 
 import { CoffeeController } from './coffee.controller';
 import { CoffeeService } from './coffee.service';
 
-class MockCoffeeService {}
+// class MockCoffeeService {}
 
-// useClass provider
-class ConfigService {}
-class DevelopmentConfigService {}
-class ProductionConfigService {}
+// // useClass provider
+// class ConfigService {}
+// class DevelopmentConfigService {}
+// class ProductionConfigService {}
+
+// useFactory provider
+@Injectable()
+export class CoffeeBrandFactory {
+  create() {
+    return ['Turky', 'Italy'];
+  }
+}
 
 @Module({
   // providers: [{ provide: CoffeeService, useValue: new MockCoffeeService() }],
   providers: [
     CoffeeService,
+    // 1) useValue
     // { provide: COFFEE_BRAND, useValue: ['Turky', 'French'] },
+    // {
+
+    // 2) useClass
+    //   provide: CoffeeService,
+    //   useClass:
+    //     process.env.NODE_ENV === 'developement'
+    //       ? DevelopmentConfigService
+    //       : ProductionConfigService,
+    // },
+
+    // 3) useFactory
     {
-      provide: CoffeeService,
-      useClass:
-        process.env.NODE_ENV === 'developement'
-          ? DevelopmentConfigService
-          : ProductionConfigService,
+      provide: COFFEE_BRAND,
+      useFactory: (coffeeFactoryBrand: CoffeeFactoryBrand) =>
+        coffeeFactoryBrand.create(),
+      inject: [CoffeeBrandFactory],
     },
   ],
   controllers: [CoffeeController],
